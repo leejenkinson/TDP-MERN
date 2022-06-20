ba75829248e19ed48d0fca09a11cec2d035e42ba
leejenkinson <lee_jenkinson@msn.com>
2022-06-20 12:48:22 +0100

    added delete and update functionality with minor css changes

--- front-end/src/trainerApi/AddNewTrainer.jsx
+++ front-end/src/trainerApi/AddNewTrainer.jsx
@@ -2,59 +2,82 @@ import axios from "axios";
 import { useState } from "react";
 
 
-const AddNewTrainer =({getData, fetchData})=>{
+const AddNewTrainer = ({ getData, fetchData }) => {
 
     //3 states
-        //name
-        //age
-        // specialism
-        const [name, setName] = useState();
-        const [age, setAge] = useState();
-        const [special, setSpecial] = useState();
-        //and id now:
-        const [id, setId] = useState();
+    //name
+    //age
+    // specialism
+    const [name, setName] = useState();
+    const [age, setAge] = useState();
+    const [special, setSpecial] = useState();
+    //and id now:
+    const [id, setId] = useState();
 
-        const handleSubmit= (e)=>{
-            e.preventDefault();
+    const handleSubmit = (e) => {
+        e.preventDefault();
 
-            axios.post("http://localhost:4494/trainers/create",{
-                "_id": id,
-                "name": name,
-                "age": age,
-                "specialism": special
-            })
-            .then((res)=>{
+        axios.post("http://localhost:4494/trainers/create", {
+            "_id": id,
+            "name": name,
+            "age": age,
+            "specialism": special
+        })
+            .then((res) => {
                 console.log(res);
 
-                setTimeout(()=>{
+                setTimeout(() => {
 
                     getData(!fetchData)
                     console.log("loading")
                 }, 500)
                 console.log("done")
             })
-            .catch((error)=>alert(error))
-        }
+            .catch((error) => alert(error))
+    }
 
-        console.log(fetchData)
+    const handleUpdate = (e) => {
+        e.preventDefault();
 
-        
-        return (
-          <form >
+        axios.put("http://localhost:4494/trainers/update/" + id, {
+            "_id": id,
+            "name": name,
+            "age": age,
+            "specialism": special
+        })
+            .then((res) => {
+                console.log(res);
+
+                setTimeout(() => {
+
+                    getData(!fetchData)
+                    console.log("loading")
+                }, 500)
+                console.log("done")
+            })
+            .catch((error) => alert(error))
+    }
+
+    console.log(fetchData)
+
+
+    return (
+        <form >
             <h4>Insert values into the following boxes: </h4>
-            <input type="number" min={0} placeholder="ID" value={id} onChange={(e)=>{setId(e.target.value)}}/>
-            <br/>
-            <input type="text" placeholder="Name" value={name} onChange={(e)=>{setName(e.target.value)}}/>
-            <br/>
-            <input type="number" min={1} max={150} placeholder="Age" value={age} onChange={(e)=>{setAge(e.target.value)}}/>
-            <br/>
-            <input type="text" placeholder="Specialism" value={special}onChange={(e)=>{setSpecial(e.target.value)}} />
-            <br/>
+            <input type="number" min={0} placeholder="ID" value={id} onChange={(e) => { setId(e.target.value) }} />
+            <br />
+            <input type="text" placeholder="Name" value={name} onChange={(e) => { setName(e.target.value) }} />
+            <br />
+            <input type="number" min={1} max={150} placeholder="Age" value={age} onChange={(e) => { setAge(e.target.value) }} />
+            <br />
+            <input type="text" placeholder="Specialism" value={special} onChange={(e) => { setSpecial(e.target.value) }} />
+            <br />
             <h4>Select POST (to add a new trainer)</h4>
-            <button className="button button1" onClick={handleSubmit} >POST</button>
-          </form>
-        );
-            
+            <button className="button1" onClick={handleSubmit} >POST</button>
+            <h4>Select UPDATE (to update trainer)</h4>
+            <button className="button2" onClick={handleUpdate} >UPDATE</button>
+        </form>
+    );
 }
 
 export default AddNewTrainer;
--- front-end/src/trainerApi/Trainers.jsx
+++ front-end/src/trainerApi/Trainers.jsx
@@ -2,8 +2,10 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import Trainer from "../Trainer";
 import AddNewTrainer from "./AddNewTrainer";
+import DeleteTrainer from "./DeleteTrainer";
+import UpdateTrainer from "./AddNewTrainer";
 
-const Trainers= () => {
+const Trainers = () => {
 
     const [trainers, setTrainers] = useState([]);
     const [fetchData, setFetchData] = useState(false);
@@ -15,17 +17,23 @@ const Trainers= () => {
             .catch(err => console.error(err));
     }, [fetchData]);
 
-    return ( 
+
+    return (
         <>
-        <h2>List of trainers: </h2>
-        {
-            trainers.map(trainer => <Trainer key={trainer._id} id={trainer._id} name={trainer.name} age={trainer.age} specialism={trainer.specialism}/>)
-        }
-        <br/>
-        <h2>Add new trainer: </h2>
-        <AddNewTrainer getData={setFetchData} fetchData={fetchData}/>
+            <div className="trainerList">
+                <h2>List of trainers: </h2>
+                {
+                    trainers.map(trainer => <Trainer key={trainer._id} id={trainer._id} name={trainer.name} age={trainer.age} specialism={trainer.specialism} />)
+                }
+            </div>
+            <br />
+            <h2>Add new trainer: </h2>
+            <AddNewTrainer getData={setFetchData} fetchData={fetchData} />
+            <br />
+            <DeleteTrainer getData={setFetchData} fetchData={fetchData} />
+
         </>
-     );
+    );
 }
 
 export default Trainers;
